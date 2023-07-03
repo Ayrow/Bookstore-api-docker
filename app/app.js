d90d0ec6e5e6da015721230cd9fd75bb7322fb91
Aymeric <aymeric.pilaert@hotmail.fr>
2023-07-03 13:30:08 +0200

    setup the book routes

--- app/app.js
+++ app/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 
+const bookRouter = require('./routes/book');
+
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use('/book', bookRouter);
 
 app.listen(3000);
 
---
+++ ddl/bookTestData.sql
@@ -0,0 +1,8 @@
+insert into book (id, author, price, description, year_published)
+values ('c2417d00-27a0-41a9-a0a8-2c62a81ddc0e', 'Test author 1', 111, 'Dummy description', 1999);
+
+insert into book (id, author, price, description, year_published)
+values ('7a4afe3d-bb59-45a0-91de-24560d8d5141', 'Test author 2', 222, 'Dummy description', 2022);
+
+insert into book (id, author, price, year_published)
+values ('14920ab4-eab2-4b6a-934e-0f189b1f626a', 'Test author 3', 333, 2007);
