ad8f07f26deb44449f1e86e89d54b5ae12728e56
Aymeric <aymeric.pilaert@hotmail.fr>
2023-07-07 10:43:33 +0200

    testing post book

--- test/routes/book.js
+++ test/routes/book.js
@@ -4,6 +4,7 @@ const chaiHttp = require('chai-http');
 
 const app = require('../../app/app');
 const client = require('../../app/db');
+const { getBook } = require('../../app/models/book');
 
 chai.use(chaiHttp);
 
@@ -36,6 +37,10 @@ describe('Testing http endpoint', () => {
     );
   }
 
+  function addBook({ testingBook, endCallback }) {
+    chai.request(app).post('/book').send(testingBook).end(endCallback);
+  }
+
   describe('Testing POST book', () => {
     before(async () => {
       await cleanup();
@@ -43,16 +48,51 @@ describe('Testing http endpoint', () => {
 
     describe('Testing adding correct book', () => {
       it('Correct response status', (done) => {
-        chai
-          .request(app)
-          .post('/book')
-          .send(testingBook1)
-          .end((_err, resp) => {
+        addBook({
+          testingBook: testingBook1,
+          endCallback: (_err, resp) => {
             expect(resp).to.have.status(201);
             expect(resp.body).to.have.property('id');
             testingBook1.id = resp.body.id;
             done();
-          });
+          },
+        });
+      });
+
+      it('Created book is equal to the testing book', async () => {
+        const book = await getBook({ bookId: testingBook1.id });
+        delete book.added_dttm;
+        expect(testingBook1).to.deep.equal(book);
+      });
+    });
+
+    describe('Testing adding book without price', () => {
+      let countBefore;
+      before(async () => {
+        const resp = await client.query(`
+        select count(*) as count
+        from book
+        `);
+        countBefore = resp.rows[0].rows;
+      });
+
+      it('Correct response status', (done) => {
+        addBook({
+          testingBook: testingBook2,
+          endCallback: (_err, resp) => {
+            expect(resp).to.have.status(400);
+
+            done();
+          },
+        });
+      });
+
+      it('Count after is equal to count befofe so no new book create', async () => {
+        const resp = await client.query(`
+        select count(*) as count
+        from book
+        `);
+        expect(resp.rows[0].rows).to.equal(countBefore);
       });
     });
   });
